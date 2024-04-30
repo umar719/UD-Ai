1a6d5bd1b04c2d124218a7e32cecb71345c195e5
U M A R D E V I L <103053269+umar719@users.noreply.github.com>
2024-05-01 01:48:25 +0500

    Create 18+.js

---
+++ plugins/18+.js
@@ -0,0 +1,44 @@
+let handler  = async (m, { conn }) => {
+	
+	await m.reply('[!] Please Wait, Wait a moment')
+  conn.reply(m.chat,`${pickRandom(global.bokep)}`, m)
+}
+handler.help = ['filebokep']
+handler.tags = ['internet']
+handler.command = /^(filebokep)$/i
+handler.limit = 1
+
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.bokep = [
+'Name : DilaPye Colmek\nFoto : -\nVideo : 1\nSize : 90 MB\nDuration : 5 Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1We9TeG1whjz2bmNCrahctK1-Hiy3BtWN/view \n PASSWORD FILE : AA18+#29',
+'Name : Missluttyv SMA\nFoto : -\nVideo : 3\nSize : 74 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1TodhXRAHt6MHMqS2-3DYmCQoIFe7yuI-/view \nPASSWORD FILE : AA18+#21',
+'Name : Arce\nFoto : 114\nVideo : 16\bSize : 64 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1SwXbmd2sXnYSNYmn_tdCiaBsHHXjYvQM/view \nPASSWORD FILE : AA18+#6',
+'Name : Annisa\nFoto : 71\nVideo : 2\nSize : 48 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1St_Pb9xNZnrSX_fPfaarno6Bpjc3ArUx/view\nPASSWORD FILE : AA18+#5',
+'Name : Annisa\nFoto : 71\nVideo : 2\nSize : 48 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1St_Pb9xNZnrSX_fPfaarno6Bpjc3ArUx/view\nPASSWORD FILE : AA18+#5',
+'Name : 038493040494_0[Ws_Gloia]\nFoto : 19\nVideo : 1\nSize : 379 MB\nDuration : 8 Minute\nQuality : HD++\nRate : ⭐⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1Y0ALFotYLT2F8kKjgxMZ_BFJ6vWKalck/view\nPASSWORD FILE : AA18+#30',
+'Name : ASD - Ria Bali\nFoto : -\nVideo : 1\nSize : 165 MB\nDuration : 45 Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1T84UWWj1oJlDAMXd-snZr6gP4Yt61P6P/view\nPASSWORD FILE : AA18+#7',
+'Name : Nanad Apr\nFoto : 18\nVideo : 23\nSize : 357 MB\nDuration : - Minute\nQuality : -\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1YO0rvEgbx6Qsrtfm6tTOlvHI7OE5V42p/view\nPASSWORD FILE : AA18+#32',
+'Name : lily kawai onlyfans\nFoto : 50\nVideo : 7\nSize : 237 MB\nDuration : - Minute\nQuality : HD+\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1TcK_EH1d2vL4bfVhSBm0el_nhmqh2XCS/view\nPASSWORD FILE : AA18+#17',
+'Name : Bocil x Tante\nFoto : -\nVideo : 1\nSize : 156 MB\nDuration : 23 Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1amwgPTf_PLIUc43bxivztFrD0xzdg8Tm/view\nPASSWORD FILE : AA18+#33',
+'Name : Badbitch_alter\nFoto : -\nVideo : 1\nSize : 170 MB\nDuration : 26 Minute\nQuality : HD+\nRate : ⭐⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1b2k9RvUeirTzfHBZJkQ9fgw0jJudGP2j/view\nPASSWORD FILE : AA18+#34',
+'Name : Princess nakal\nFoto : -\nVideo : 1\nSize : 128 MB\nDuration : 15 Minute\nQuality : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1bj6nNKADyIzSXONZnmXs7c8E_Ln_3KlM/view\nPASSWORD FILE : AA18+#35',
+'Name : weweci\nFoto : -\nVideo : 1\nSize : 180 MB\nDuration : 21 Menit\nMinute : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1cA-pPAB_WRGRRGxhBDk9ouDiynu17xPg/view\nPASSWORD FILE : AA18+#36',
+'Name : Viral Katanya\nFoto : -\nVideo : 1\nSize : 26 MB\nDuration : 5 Minute\nKualitas : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : http://www.mediafire.com/file/9o80ka9z61cdtn1/%23Viral_katanya.zip/file',
+'Name : Hijab hitam\nFoto : -\nVideo : 4\nSize : 20 MB\nDuration : - Minute\nKualitas : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1gM7UvH1jzxYek88mQnWBQmou78Gerb_w/view',
+'Name : Syifa\nFoto : 45\nVideo : 25\nSize : 100 MB\nDuration : - Minute\nKualitas : HD+\nRate : ⭐⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1gTjGotGSlpRCZiLKaZ14sOLjBjirntUb/view\nPassword file : AA18+#27',
+'Name : Danca\nFoto : 18\nVideo : 13\nSize : 100 MB\nDuration : - Minute\nKualitas : HD+\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1hDxgMnQ4ps2DkVhFyc51ApdsavGfqs3B/view\nPassword file : AA18+#38',
+'Name : Dewi\nFoto : 266\nVideo : 3\nSize : 150 MB\nDurationi : - Minute\nKualitas : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1iVfhweyoExOcSCq-LHvJSHRp7pmbNmzg/view\nPassword file : AA18+#40',
+'Name : Jenn\nFoto : 50\nVideo : 7\nSize : 105 MB\nDuration : - Minute\nKualitas : HD+\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1jqYvhwGLp8BgTItHRQpONu1MX3o4o12d/view\nPassword file : AA18+#41',
+'Name : Yanna Davud\nFoto : 61\nVideo : 5\nSize : 22 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1kIs0lNpZW-R5ub09GiQG6-wgdfagoZsV/view\nPassword file : AA18+#41',
+'Name : Nina fika luna\nFoto : 30\nVideo : 28\nSize : 153 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1kirVFR8V7aYAvY_6s9v5QKyxy5hRQw-X/view\nPassword file : AA18+#42',
+'Name : Ain Penang\nFoto : 53\nVideo : 7\nSize : 42 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1kzGeQKuKaQKiz6u_JEcPh9wuJGKVB-sv/view\nPassword file : AA18+#43',
+'Name : Jilboobs\nFoto : -\nVideo : 8\nSize : 84 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1l6M6JXmNiHGaybQiHH39wucVhvLkTjkX/view\nPassword file : AA18+#44',
+'Name : Kiki hijab\nFoto : -\nVideo : 14\nSize : 190 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1lZDvGBclp35JxhWRpVSCrVOO5YhIQSSn/view\nPassword file : AA18+#45',
+'Name : Tifanny\nFoto : 117\nVideo : 4\nSize : 30 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1lrO9YJ1-MJtHavMpN2kG3nmJSReJtzE6/view\nPassword file : AA18+#46',
+'Name : Vierannii\nFoto : 95\nVideo : -\nSize : 12 MB\nDuration : - Minute\nQuality : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1lrOIZFT1n5XWqkX5uVq7xmmSpKjX70Mf/view\nPassword file : AA18+#47',
+]
